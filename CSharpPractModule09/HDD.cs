db708fe7f21eee5b83db82745f8badda81c675ba
NajaNaja <NajaNaja89@gmail.com>
2018-10-06 20:01:13 +0600

    base class

---
+++ CSharpPractModule09/HDD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractModule09
+{
+    public class HDD : Storage
+    {
+        protected HDD():base
+        {
+           
+        }
+
+        public int SpindleSpeed { set; get; }
+
+        
+    }
+}
---
+++ CSharpPractModule09/Storage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractModule09
+{
+    public abstract class Storage
+    {
+        public string NameStorage { set; get; }
+        public string ModelStorage { set; get; }
+
+        public double storageCapacity = 0;
+
+        public abstract double GetStorageCapacity();
+        public abstract void CopyToStorage();
+        public abstract double GetStorageFreeCapacity();
+        public abstract string GetStorageInfo();
+
+    }
+}
